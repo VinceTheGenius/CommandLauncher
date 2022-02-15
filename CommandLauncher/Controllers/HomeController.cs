6e6c02e981e41c1832b10ccde51aed13ad351b0f
Vincent Pruvost <val.pruvost@hotmail.com>
2022-02-15 10:20:11 +0100

    Initialisation du projet

---
+++ CommandLauncher/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CommandLauncher.Models;
+
+namespace CommandLauncher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Launcher(string text)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Launch(string Command)
+        {
+            var fileName = "C:/VincentTheGenius/afficher.txt";
+
+            await System.IO.File.WriteAllTextAsync(fileName, Command);
+
+            System.Diagnostics.Process proc = new System.Diagnostics.Process(); //Declare le nouveau process
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.FileName = Command;
+            //proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            proc.WaitForExit();
+
+            string output = proc.StandardOutput.ReadToEnd();
+
+            return RedirectToAction("Launcher", new { text = output });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CommandLauncher/Models/CommandModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandLauncher.Models
+{
+    public class CommandModel
+    {
+        public string Command { get; set; }
+    }
+}
